38769623cf535796beaed0352434d0a40575aa29
akosENagy <akos.e.nagy@gmail.com>
2017-03-29 14:12:40 +0200

    stagehandler fixed

--- stage_handler.py
+++ stage_handler.py
@@ -1,14 +1,13 @@
-def makeStage():
-    # List, what contains numbers, between 1 and 9 and then shuffle it.
-    l = [x for x in range(1, 10)]
-    shuffle(l)
+from random import shuffle
+from random import randint as rnd
 
-    # The Sudoku table
+
+def makeStage():
+    l = [x for x in range(1, 10)]
     m = [[None for _ in range(9)] for _ in range(9)]
+    shuffle(l)
     y, x = 0, 0
     k = 0
-
-    # The upperleft block
     for i in range(3):
         for i in range(3):
             m[y][x] = l[k]
@@ -16,68 +15,69 @@ def makeStage():
             k += 1
         x = 0
         y += 1
-
-    # The uppermiddle block
-    x, y = 0, 0
-    for i in range(3):
-        m[y][3 + x] = m[1 + y][x]
-        x += 1
+    # idaáig
     x = 0
-    for i in range(3):
-        m[1 + y][3 + x] = m[2 + y][x]
-        x += 1
-    y, x = 0, 0
-    for i in range(3):
-        m[2 + y][3 + x] = m[y][x]
-        x += 1
-
-    # The upperright block
     y = 0
     for i in range(1):
         for i in range(3):
-            m[y][6 + x] = m[1 + y][x]
+            m[0 + y][3 + x] = m[1 + y][0 + x]
+            x += 1
+        y += 0
+        x = 0
+        for i in range(3):
+            m[1 + y][3 + x] = m[2 + y][0 + x]
+            x += 1
+        y, x = 0, 0
+        for i in range(3):
+            m[2 + y][3 + x] = m[0 + y][0 + x]
+            x += 1
+    x = 3
+    y = 0
+    for i in range(1):
+        for i in range(3):
+            m[0 + y][3 + x] = m[1 + y][0 + x]
             x += 1
         y = 0
         x = 3
         for i in range(3):
-            m[1 + y][6 + x] = m[2 + y][x]
+            m[1 + y][3 + x] = m[2 + y][0 + x]
             x += 1
         y, x = 0, 3
         for i in range(3):
-            m[2 + y][6 + x] = m[y][x]
+            m[2 + y][3 + x] = m[0 + y][0 + x]
             x += 1
 
-    # The middle columns, what copies coordinates
-    y, x = 0, 0
+    y = 0
+    x = 0
+    # középső oszlopok
     for i in range(3):
         for i in range(3):
-            m[3 + y][2 + x] = m[y][x]
+            m[3 + y][2 + x] = m[0 + y][0 + x]
             y += 1
         y -= 3
         for i in range(3):
-            m[3 + y][0 + x] = m[y][1 + x]
+            m[3 + y][0 + x] = m[0 + y][1 + x]
             y += 1
         y -= 3
         for i in range(3):
-            m[3 + y][1 + x] = m[y][2 + x]
+            m[3 + y][1 + x] = m[0 + y][2 + x]
             y += 1
         y -= 3
         x += 3
-
-    # The lower columns, what copies coordinates
     x = 0
     y = 3
+    # lsó oszlopok
     for i in range(3):
         for i in range(3):
-            m[3 + y][2 + x] = m[y][x]
+            m[3 + y][2 + x] = m[0 + y][0 + x]
             y += 1
         y -= 3
         for i in range(3):
-            m[3 + y][0 + x] = m[y][1 + x]
+            m[3 + y][0 + x] = m[0 + y][1 + x]
             y += 1
         y -= 3
         for i in range(3):
-            m[3 + y][1 + x] = m[y][2 + x]
+            m[3 + y][1 + x] = m[0 + y][2 + x]
             y += 1
         y -= 3
         x += 3
@@ -85,7 +85,7 @@ def makeStage():
 
 
 # At random coordinates removes the number
-def initBoard(m):
+def initBoard(m, difficulty, z):
     already_chosen = []
 
     for i in range(difficulty * 7):
