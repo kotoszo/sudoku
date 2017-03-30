300726bcf9ac84fc9d352cdbe8a224ba057f56f7
rtomil <rtomilobil@gmail.com>
2017-03-30 13:33:44 +0200

    Done. like never before

--- Sudoku_1.py
+++ Sudoku_1.py
@@ -2,8 +2,6 @@
 # CODECOOL 2017                             #
 #
 
-
-#
 # IMPORTS                           #
 from output_handler import *
 from stage_handler import *
@@ -13,37 +11,39 @@ from os import system
 import getch
 import vlc
 
-#
-# MAIN                                 #
 
-
+# Start                               #
 def main():
     wannaplay = True
     highlighted = [0, 0]
-
+    # As long as you want to play
     while wannaplay:
 
         moves = []
-        difficulty = welcomeScreen()
+        difficulty = welcome_screen()
+        # List for the numbers what we took out.
         z = [[False for _ in range(9)] for _ in range(9)]
-        m = initBoard(makeStage(), difficulty, z)
+        m = init_board(makeStage(), difficulty, z)
         system('clear')
 
-        while not playerHasWon(m):
+        while not player_has_won(m):
 
+            # The belonging number for the givin coordinates, gonna be colored.
+            # That means, the exact same number gonna be colored, where the player "stands"
             m[highlighted[0]][highlighted[1]] = bcolors['YELLOW'] + \
                 str(m[highlighted[0]][highlighted[1]]) + bcolors['ENDC']
-            printGameState(m)
-            print()
-            highlighted = getInput(m, z, moves, highlighted)
+
+            print_game_state(m)
+            highlighted = get_input(m, z, moves, highlighted)
 
         system('clear')
-        printGameState(m)
+        print_game_state(m)
         print("\nCongratulations! You won!")
         vlc.MediaPlayer("yeah.wav").play()
+
         print("Wanna play again? 'y' for yes, anything else to quit.")
-        stillWantsToPlay = getch.getch()
-        if stillWantsToPlay.lower() != 'y':
+        still_wants_to_play = getch.getch()
+        if still_wants_to_play.lower() != 'y':
             wannaplay = False
 
 
