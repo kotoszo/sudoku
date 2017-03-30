29eadbb4a6223b93216a7ff988b919c885b1704a
akosENagy <akos.e.nagy@gmail.com>
2017-03-30 13:29:13 +0200

    cleaned up the code a bit.

--- status_handler.py
+++ status_handler.py
@@ -1,30 +1,27 @@
 
-def subMatrixesAreClean(matrix):
+def sub_matrixes_are_clean(matrix):
     # Checks whether every submatrix is clean on the board
-    clean = True
 
     for y in range(0, 7, 3):
         for x in range(0, 7, 3):
 
             subMatrix = [matrix[y + j][x + i] for i in range(3) for j in range(3)]
 
-            if len(subMatrix) != len(set(subMatrix)):
-                clean = False
+            if len(subMatrix) != len(set(subMatrix)):  # Checks duplicates
+                return False
 
-            for i in range(1, 10):
+            for i in range(1, 10):  # Checks whether every number is present
                 if i not in subMatrix:
-                    clean = False
+                    return False
 
-    return clean
+    return True  # If it reached this far, it's clean.
 
 
-def playerHasWon(matrix):
-
-    won = True
+def player_has_won(matrix):
 
     for row in matrix:
         for item in row:
             if item == '.':
                 return False
 
-    return won
+    return True
